2583cba8f761fc086784eafb8ef274f8eb432068
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-08 11:00:58 -0500

    Exercises

---
+++ arrayslists/demolists/exercises.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSharp1Exercises.ArraysAndLists
+{
+    public class Lists
+    {
+        /// <summary>
+        /// Write a program and continuously ask the user to enter different names, until the user presses Enter 
+        /// (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
+        /// </summary>
+        public void Exercise1()
+        {
+            var names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Type a name (or hit ENTER to quit): ");
+
+                var input = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+                names.Add(input);
+            }
+
+            if (names.Count > 2)
+                Console.WriteLine("{0}, {1} and {2} others like your post", names[0], names[1], names.Count - 2);
+            else if (names.Count == 2)
+                Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
+            else if (names.Count == 1)
+                Console.WriteLine("{0} likes your post.", names[0]);
+            else
+                Console.WriteLine();
+        }
---
+++ arrayslists/demolists/program.cs
@@ -0,0 +1,14 @@
+using system.Collections.Generic;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int);
+        }
+    }
+}
