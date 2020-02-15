d4c4a877f6f08a66b339cc0115ca4a6c0d6da018
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-15 13:42:57 -0500

    directoryinfo

---
+++ workingwfiles/program2.cs
@@ -0,0 +1,14 @@
+using system;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+        }
+    }
+}
