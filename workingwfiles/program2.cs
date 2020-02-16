d89d53f194ff09cb6ab55afba2843ea80f3b99c8
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-16 13:12:20 -0500

    debugtools

--- workingwfiles/program2.cs
+++ workingwfiles/program2.cs
@@ -9,6 +9,17 @@ namespace CSharpFundamentals
             Directory.CreateDirectory(@"c:\temp\folder1");
 
             var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\CSharpFundamentals, "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            Console.WriteLine(directory);
+
+    var directoryInfo = new directoryInfo("...");
+    directoryInfo.GetFiles();
+    directoryInfo.GetDirectories();
+
         }
     }
 }
