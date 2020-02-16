5e21814d0b4b66b850947b8bf03e992b23db2d71
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-16 14:36:18 -0500

    finished

--- debugging/program.cs
+++ debugging/program.cs
@@ -9,19 +9,25 @@ namespace CSharpFundamentals
            var numbers = new List<int>{ 1, 2, 3, 4, 5, 6};
            var smallests = GetSmallests(numbers, 3);
 
-           foreach (var number in smallests)
+           foreach (var number in numbers)
                 Console.WriteLine(number);
             
         }
         public static List<int> GetSmallests(List<int> list, int count)
         {
+            if(list.Count == null)
+                throw new ArgumentOutOfRangeException("count", "count no no no");
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "count cant be greater than num of elements");
+
+            var buffer = new List<int>(list);
             var smallests = new List<int>();
 
             while (smallests.Count < count)
             {
-                var min = GetSmallests(list);
+                var min = GetSmallests(buffer);
                 smallests.Add(min);
-                list.Remove(min);
+                buffer.Remove(min);
             }
             return smallests;
         }
